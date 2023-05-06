e6bfe2688d47f5b4135b342439c6f710b00ec5db
Yuttakhan B <yuttakhanbaingen@gmail.com>
2023-05-06 11:23:28 +0700

    :sparkles: Apply interface to view

--- src/extension.ts
+++ src/extension.ts
@@ -2,17 +2,13 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import LocalAIViewProvider from "./local-ai-view-provider";
+import { LocalAI } from "./local-ai-interface";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  const localAiViewProvider = new LocalAIViewProvider(context);
-
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "localai-vscode-plugin" is now active!'
-  );
+  const localAI = new LocalAI("http://localhost:8080", "ggml-gpt4all-j");
+  const localAiViewProvider = new LocalAIViewProvider(context, localAI);
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
@@ -41,13 +37,10 @@ export function activate(context: vscode.ExtensionContext) {
           selection.end.character
         );
         const highlighted = editor.document.getText(selectionRange);
-
         localAiViewProvider.sendMessageToWebView({
           command: "hilighted",
           text: highlighted,
         });
-        // await context.globalState.update('local-ai-hilighted-text', highlighted)
-        // vscode.window.showInformationMessage("text", highlighted);
       }
     }
   );
